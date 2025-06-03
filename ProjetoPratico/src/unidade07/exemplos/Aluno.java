dd023d89fff423beefec6ebdad48a8260e93f601
André <andre@iburger.com.br>
2025-06-03 18:20:27 -0300

    🧪 exemplos: Unidade 07

---
+++ ProjetoPratico/src/unidade07/exemplos/Aluno.java
@@ -0,0 +1,17 @@
+package unidade07.exemplos;
+
+public class Aluno extends Object {
+    
+    int codigoMatricula;
+    String nome;
+
+    public Aluno() {
+        super();
+    }
+
+    public Aluno(int codigoMatricula, String nome) {
+        this.codigoMatricula = codigoMatricula;
+        this.nome = nome;
+    }    
+ 
+}
---
+++ ProjetoPratico/src/unidade07/exemplos/Sistema.java
@@ -0,0 +1,14 @@
+package unidade07.exemplos;
+
+
+public class Sistema {
+
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno();
+        Aluno aluno2 = new Aluno(123, "André");
+
+        aluno.codigoMatricula = 1234;
+        
+        
+    }
+}
